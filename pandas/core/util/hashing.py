2d839d7ff9dacb0b1de40ef0156ac915f625ecf2
Dawid Makar <dawid.makar@acaisoft.com>
2023-09-27 01:09:54 +0200

    Automatic refactoring. Refactoring step id: UUID('6454e6f2-648d-4429-a580-ff8c6d8c5227')

--- pandas/core/util/hashing.py
+++ pandas/core/util/hashing.py
@@ -254,7 +254,7 @@ def hash_array(
             encoding=encoding, hash_key=hash_key, categorize=categorize
         )
 
-    if not isinstance(vals, np.ndarray):
+    elif not isinstance(vals, np.ndarray):
         # GH#42003
         raise TypeError(
             "hash_array requires np.ndarray or ExtensionArray, not "
@@ -275,15 +275,14 @@ def _hash_ndarray(
     """
     dtype = vals.dtype
 
-    # _hash_ndarray only takes 64-bit values, so handle 128-bit by parts
+    # we'll be working with everything as 64-bit values, so handle this
+    # 128-bit value early
     if np.issubdtype(dtype, np.complex128):
-        hash_real = _hash_ndarray(vals.real, encoding, hash_key, categorize)
-        hash_imag = _hash_ndarray(vals.imag, encoding, hash_key, categorize)
-        return hash_real + 23 * hash_imag
+        return hash_array(np.real(vals)) + 23 * hash_array(np.imag(vals))
 
     # First, turn whatever array this is into unsigned 64-bit ints, if we can
     # manage it.
-    if dtype == bool:
+    elif dtype == bool:
         vals = vals.astype("u8")
     elif issubclass(dtype.type, (np.datetime64, np.timedelta64)):
         vals = vals.view("i8").astype("u8", copy=False)
